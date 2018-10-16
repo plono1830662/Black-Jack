c69d6720f7bbed5f82dc4e0e9a8567bb37a95252
plono1830662 <plono1830662@etu.cegepjoquiere.ca>
2018-10-16 18:21:11 -0400

    Ajoutez des fichiers projet.

---
+++ black jack final version/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace black_jack_final_version
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
